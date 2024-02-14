30d10c0acfe14cedd0ba6e6cdc5ea4f5240eec57
Pranav <pranava@metadome.ai>
2024-02-14 17:57:50 +0530

    Added swagger to existing api's.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './exception_filters/http.exception';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('E-Commerce API example')
+    .setDescription('The E-Commerce API description')
+    .setVersion('1.0')
+    .addTag('E-Commerce')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(8080);
 }
